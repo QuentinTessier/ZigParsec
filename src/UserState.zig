2ed179532ccab5853119ee7734b11c80f87d0765
QuentinTessier <tessier.quentin.pro@gmail.com>
2026-04-27 23:08:27 +0200

    clean

--- src/UserState.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-
-pub const State = struct {
-    should_collect_value: bool = true,
-    should_collect_error: bool = true,
-    auto_eat_whitespace: bool = false,
-
-    verbose: bool = true,
-};
--- src/utils.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-
-pub fn ReturnType(comptime Fn: type) type {
-    return switch (@typeInfo(Fn)) {
-        .@"fn" => |f| f.return_type.?,
-        .pointer => |p| ReturnType(p.child),
-        else => @compileError("Expected a function got " ++ @typeName(Fn)),
-    };
-}
